1328280cd03cdfe880d244eab8cf831ce747fb17
joaoooDias <joao_motta1@hotmail.com>
2024-11-27 12:28:00 -0300

    Continuação do projeto

--- src/App.js
+++ src/App.js
@@ -7,6 +7,145 @@ import Estado from './componentes/Estado';
 
 function App() {
 
+  const estado = [
+    {
+      nome: 'Acre',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Alagoas',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Amapá',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Amazonas',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Bahia',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Ceará',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Distrito Federal',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Espírito Santo',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Goiás',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Maranhão',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Mato Grosso',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Mato Grosso do Sul',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Minas Gerais',
+      corPrimaria: '#F0F8E2',
+      corSecundaria: '#A6D157'
+    },
+    {
+      nome: 'Pará',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Paraíba',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Paraná',
+      corPrimaria: '#FAE9F5',
+      corSecundaria: '#DB6EBF'
+    },
+    {
+      nome: 'Pernambuco',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Piauí',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Rio de Janeiro',
+      corPrimaria: '#E8F8FF',
+      corSecundaria: '#82CFFA'
+    },
+    {
+      nome: 'Rio Grande do Norte',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Rio Grande do Sul',
+      corPrimaria: '#FDE7E8',
+      corSecundaria: '#E06B69'
+    },
+    {
+      nome: 'Rondônia',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Roraima',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Santa Catarina',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'São Paulo',
+      corPrimaria: '#D9f7E9',
+      corSecundaria: '#57C278'
+    },
+    {
+      nome: 'Sergipe',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Tocantins',
+      corPrimaria: '',
+      corSecundaria: ''
+    }
+    
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -17,12 +156,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      <Estado nome="São Paulo"/>
-      <Estado nome="Minas Gerais"/>
-      <Estado nome="Rio de Janeiro"/>
-      <Estado nome="Rio Grande do Sul"/>
-      <Estado nome="Paraná"/>
-      <Estado nome=""/>
+      {estado.map(estado => <Estado key={estado.nome} nome={estado.nome} corPrimaria={estado.corPrimaria} corSecundaria={estado.corSecundaria}/>)}
     </div>
   );
 }
--- src/componentes/Estado/index.js
+++ src/componentes/Estado/index.js
@@ -1,9 +1,13 @@
 import './Estado.css'
 
 const Estado = (props) => {
+
+    const css = {backgroundColor: props.corPrimaria}
+
     return (
-        <section className="estado">
-            <h3>{props.nome}</h3>
+        
+        <section className='estado' style={css}>
+            <h3 style={{ borderColor: props.corSecundaria}}>{props.nome}</h3>
         </section>
     )
 }
